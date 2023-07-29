f232b0bbbc19c01a130f8f15824d3975600bdd8a
erinelise2 <erinelise2@gmail.com>
2023-07-29 08:43:45 -0700

    updating conservation page

--- erinFinalProject/src/Pages/ConservationGroups.js
+++ erinFinalProject/src/Pages/ConservationGroups.js
@@ -9,12 +9,12 @@ export default function ConservationGroups() {
     <>
     <div className="conservation-container">
       <h1 className="conservation-header mt-3 mb-3">Conservation Groups</h1>
-      <h4 className="conservation-text">Click on the links below to learn more about some of the amazing organizations working to protect our wildlife all around our world!</h4>
       <Row>
           <Col>
             <Image src={conservation} className="img-fluid mb-4" rounded alt="Unless Quote inside a drawing of the world with handdrawn animal around the world"></Image>
           </Col>
-        </Row>
+      </Row>
+      <h4 className="conservation-text">Click on the links below to learn more about some of the amazing organizations working to protect our wildlife all around our world!</h4>
       <ul className="conservation-list">
         <li><a href="https://www.nature.org/en-us/" className="con-link">The Nature Conservancy</a></li>
         <li><a href="https://gorillafund.org/" className="con-link">Dian Fossey Gorilla Fund</a></li>
