ff060f4c4bfad630f9ccfc26cfbbe972e80a0167
erinelise2 <erinelise2@gmail.com>
2023-07-30 08:55:46 -0700

    styled footer and added hr

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,9 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <p className="footer-text mt-4 ms-2">final project created by erin elise</p>
+        <>
+        <hr className='mt-4 text-white border-3 border-white'></hr>
+        <p className="footer-text ms-2">final project created by erin elise</p>
+        </>
     )
 }
