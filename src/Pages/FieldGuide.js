65fd6b8dbf0105fad25b728061a63c0f98d56e5b
erinelise2 <erinelise2@gmail.com>
2023-07-30 09:18:59 -0700

    styling field guide page

--- src/Pages/FieldGuide.js
+++ src/Pages/FieldGuide.js
@@ -36,28 +36,29 @@ export default function FieldGuide() {
 
   return (   
     <> 
-    <div className="form-container fieldguide-app">
-      <h1 className="fieldguide-header">Your Field Notes</h1>
+    <div className="form-container fieldguide-app border border-white border-3 pt-4 p-2 mt-4">
+      <h1 className="fieldguide-header mb-4">Your Field Notes</h1>
       <Container>
         <Row>
           <Col>
-            <Image src={monarch} className="img-fluid m-1" rounded alt="image of monarch on milkweed"></Image>
+            <Image src={monarch} className="img-fluid m-1 border border-white border" rounded alt="image of monarch on milkweed"></Image>
           </Col>
           <Col>
-            <Image src={lizard} className="img-fluid m-1" rounded alt="image of fence lizard on white fence"></Image>
+            <Image src={lizard} className="img-fluid m-1 border border-white border" rounded alt="image of fence lizard on white fence"></Image>
           </Col>
           <Col>
-            <Image src={blackbear} className="img-fluid m-1" rounded alt="image of black bear in forest"></Image>
+            <Image src={blackbear} className="img-fluid m-1border border-white border" rounded alt="image of black bear in forest"></Image>
           </Col>
         </Row>
       </Container>
-      <p className="fieldguide-text fs-4 fw-bold">This Field Guide keeps track of all of the animals you have seen.</p>
-      <p className="fieldguide-instructions fw-bold text-center"> You can edit or delete your observations using the buttons on the table.</p>
+      <div>
+      <p className="fieldguide-text fs-5 fw-bold mt-4">This Field Guide keeps track of all of the animals you have seen.</p>
+
       {/* I need the table headers here and then append the data to the rows.... */}
       {observations.map((observation, index) => (
         <div className="mapContainer" key={index}>
           <div>
-          <table className="fieldguide border table table-bordered table-striped table-responsive table-hover">
+          <table className="fieldguide border border-white border-3 table table-bordered table-striped table-responsive table-hover">
           <thead className="fieldguide-head fs-5 fw-bold border">
             {/* this needs to move above the mapContainer div to append each into a table, but when I do that, the table cells do not reach the same widths and the background is wrong. */}
             <tr>
@@ -85,12 +86,16 @@ export default function FieldGuide() {
             </tr>
           </tbody>
           </table>
+          
           </div>
 
         </div>
       ))}
     </div>
     <div>
+    <hr className="me-3 ms-3 border border-white"></hr>
+    <p className="fw-bold text-center text-white"> You can edit or delete your observations using the buttons on the table.</p>
+    </div>
     </div>
     </>
   );
