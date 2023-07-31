d0be8edfde0a5492d87c88d08eaef1adb2c86a57
erinelise2 <erinelise2@gmail.com>
2023-07-30 20:44:19 -0700

    added a link to the 404 page

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,8 +1,15 @@
+import {Link} from 'react-router-dom'
+
 export default function NotFound() {
     return (
+        <>
         <div className="form-container border border-white border-3">
             <h1 className="mt-2 p-2 fw-bold">Oops! Page Not Found.</h1>
             <p className="text-white fs-4 p-2 fw-bold">But that is okay, please click on one of the pages above to return to the application.</p>
         </div>
+        <div className="text-center mt-4">
+        <Link className="link text-white border border-2 p-2 rounded" to="/">Return Home</Link>
+    </div>
+        </>
     )
 }
