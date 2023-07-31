7f9c2f94088004e3572549279ea2d6b438598f6c
erinelise2 <erinelise2@gmail.com>
2023-07-30 22:32:16 -0700

    styling 404

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -4,12 +4,13 @@ export default function NotFound() {
     return (
         <>
         <div className="form-container border border-white border-3">
-            <h1 className="mt-2 p-2 fw-bold">Oops! Page Not Found.</h1>
-            <p className="text-white fs-4 p-2 fw-bold">But that is okay, please click on one of the pages above to return to the application.</p>
-        </div>
-        <div className="text-center mt-4">
+            <h1 className="mt-4 pe-2 ps-2 fw-bold">Oops! Page Not Found.</h1>
+            <p className="text-white fs-4 ps-2 pe-2 fw-bold mb-4">But that is okay, please click on the button below to return to the application.</p>
+        <div className="text-center mt-2 mb-4">
         <Link className="link text-white border border-2 p-2 rounded" to="/">Return Home</Link>
-    </div>
+        </div>
+        </div>
+
         </>
     )
 }
