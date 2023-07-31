ff695a0696d5f9e7449862b779d47463e4aa52e9
erinelise2 <erinelise2@gmail.com>
2023-07-31 08:20:19 -0700

    random fox photo generator notes on 404 page

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,5 +1,13 @@
 import {Link} from 'react-router-dom'
 
+// Random fox photo API
+// let FOX_API_URL = 'https:\/\/randomfox.ca\/?i=106'; 
+
+// $.get(FOX_API_URL, (data)=> {
+//     console.log(data);
+//     $('.fox-pics').append(`<p> ${data.pic} </p>`);
+// });
+
 export default function NotFound() {
     return (
         <>
@@ -10,8 +18,9 @@ export default function NotFound() {
         <Link className="link text-white border border-2 p-2 rounded" to="/">Return Home</Link>
         </div>
         </div>
-        {/* <div className="form-container border border-white border-3">
-            <h4 className="mt-4 pe-2 ps-2 fw-bold">While you are here, enjoy some random facts about cats... </h4>
+        {/* <div className="form-container border border-white border-3 box">
+            <h4 className="mt-4 pe-2 ps-2 fw-bold">While you are here, enjoy some random photos of foxes... </h4>
+            <div class="fox-pics"></div>
         </div> */}
         </>
     )
