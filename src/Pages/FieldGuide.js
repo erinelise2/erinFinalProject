4e4e450dadd0ff1d9150dbbbd6f6cc2ae69fe9c3
erinelise2 <erinelise2@gmail.com>
2023-08-01 12:46:30 -0700

    styled fieldguide table

--- src/Pages/FieldGuide.js
+++ src/Pages/FieldGuide.js
@@ -55,8 +55,8 @@ export default function FieldGuide() {
       </Container>
       <div>
       <p className="fieldguide-text fs-5 fw-bold mt-4">This Field Guide keeps track of all of the animals you have seen.</p>
-      <Link className="link text-white border border-2 p-2 rounded" to="/updateform">✐ Edit An Entry</Link>
-      <div class="mapContainer row">
+      <Link className="link text-white border border-2 p-2 rounded me-2" to="/updateform">✐ Edit An Entry</Link>
+      <Link className="link text-white border border-2 p-2 rounded" to="/">Return Home To Add A New Entry</Link>      <div class="mapContainer row">
         <table className="fieldguide border border-white border-3 table table-bordered table-striped table-responsive table-hover table-success mt-4">
           <thead className="fieldguide-head fs-5 fw-bold border border-white">
           <tr>
@@ -73,7 +73,7 @@ export default function FieldGuide() {
           <tbody>
               {observations.map((observation, index) => (
                 <tr key={index}>
-                  {/* <td><Button className="fieldguide-btn btn border-secondary mb-2 mt-2 border-white text-white fw-bolder" color="outline" type="submit"><Link className="text-dark link-btn" to="/hidden">✐</Link></Button></td> */}
+                  {/* <td><Button className="fieldguide-btn btn border-secondary mb-2 mt-2 border-white text-white fw-bolder" color="outline" type="submit">✐</Button></td> */}
                   <td>{observation.observation}</td>
                   <td>{observation.classification}</td>
                   <td>{observation.location}</td>
